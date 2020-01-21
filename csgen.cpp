7d0501550f1b00f119be6c0613c7e5ee4ea7a805
Dj-Polyester <45545087+Dj-Polyester@users.noreply.github.com>
2020-01-21 21:54:24 +0300

    Add files via upload

---
+++ csgen.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char **argv)
+{
+    std::ofstream out;
+    size_t len = 0;
+    while (argv[1][len])
+        ++len;
+
+    char *fullfilename = new char[len + 4];
+
+    for (size_t i = 0; i < len; i++)
+    {
+        fullfilename[i] = argv[1][i];
+    }
+    fullfilename[len] = '.';
+    fullfilename[len + 1] = 'c';
+    fullfilename[len + 2] = 's';
+    fullfilename[len + 3] = '\0';
+
+    std::cout << fullfilename << std::endl;
+
+    out.open(fullfilename, std::ios_base::app);
+
+    out << "using System;" << std::endl;
+    out << "using System.Collections.Generic;" << std::endl;
+    out << "using System.Linq;" << std::endl;
+    out << "using System.Text;" << std::endl;
+    out << "using System.Threading.Tasks;" << std::endl;
+
+    out << std::endl;
+    out << "namespace " << argv[1] << std::endl;
+    out << "{" << std::endl;
+
+    out << "\tclass Program" << std::endl;
+    out << "\t{" << std::endl;
+
+    out << "\t\tstatic void Main(string[] args)" << std::endl;
+    out << "\t\t{" << std::endl;
+    out << std::endl;
+    out << "\t\t}" << std::endl;
+
+    out << "\t}" << std::endl;
+
+    out << "}" << std::endl;
+
+    out.close();
+    delete fullfilename;
+
+    return 0;
+}
